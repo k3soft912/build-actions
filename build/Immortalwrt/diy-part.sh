82c464a9fdc845864c53e69e7f972d9c37c144e9
k3soft <123430430+k3soft912@users.noreply.github.com>
2025-11-08 10:15:54 +0800

    Update diy-part.sh

--- build/Immortalwrt/diy-part.sh
+++ build/Immortalwrt/diy-part.sh
@@ -12,7 +12,7 @@
 #sed -i '$a src-git neobird https://github.com/thinktip/luci-theme-neobird.git' feeds.conf.default
 #git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone https://github.com/Myoko/openwrt-packages package/openwrt-packages
-#git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/thinktip/luci-theme-neobird package/luci-theme-neobird
 git clone https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
 git clone https://github.com/0x676e67/luci-theme-design package/luci-theme-design
