15e9ac30f784a6cf20cbc556f3fcc0a1b6884927
k3soft <123430430+k3soft912@users.noreply.github.com>
2026-02-27 15:41:44 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -12,13 +12,13 @@
 #sed -i '$a src-git neobird https://github.com/thinktip/luci-theme-neobird.git' feeds.conf.default
 #git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git clone https://github.com/Myoko/openwrt-packages package/openwrt-packages
-#git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
-#git clone https://github.com/thinktip/luci-theme-neobird package/luci-theme-neobird
-#git clone https://github.com/AngelaCooljx/luci-theme-material3 package/luci-theme-material3
-#git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
-#git clone https://github.com/0x676e67/luci-theme-design package/luci-theme-design
-#git clone https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
-#git clone https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+git clone https://github.com/thinktip/luci-theme-neobird package/luci-theme-neobird
+git clone https://github.com/AngelaCooljx/luci-theme-material3 package/luci-theme-material3
+git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
+git clone https://github.com/0x676e67/luci-theme-design package/luci-theme-design
+git clone https://github.com/0x676e67/luci-app-design-config package/luci-app-design-config
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
 #git clone https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 #git clone https://github.com/kiddin9/luci-app-dnsfilter package/luci-app-dnsfilter
 
